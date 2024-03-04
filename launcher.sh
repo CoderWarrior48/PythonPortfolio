02ccfde92fd13a231007edd9fa2440cf83318bb0
DH <77896436+CoderWarrior48@users.noreply.github.com>
2024-03-04 18:05:02 +0000

    added multiple choice launcher

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-echo 'Enter the folder name of the project you want to run'
-read program
-$program/launcher.sh
+RED=$(tput setaf 1)
+NORMAL=$(tput sgr0)
+
+
+MYVAR=$(dialog --clear \
+--title "PythonPortfolio" \
+--menu "Choose one of the following programs:" 15 40 4 \
+            1 "ProceduralRoomGeneration" \
+            2 "RaspberryPiFanControl 2" \
+--output-fd 1)
+clear
+case $MYVAR in
+        1)
+            echo "Starting... PoceduralRoomGeneration"
+            ./ProceduralRoomGeneration/launcher.sh
+            ;;
+        2)
+            echo "${RED}Error${NORMAL}: Sorry, this program cannot be run on this device, refer to the README.md file"
+            ;;
+esac
